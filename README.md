1f8dbc5588eeeb0a6c1e2f14fb766e685dda4889
JhomeWu <qu4qu426com123@gmail.com>
2024-01-16 18:01:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dev Develop Required container
+
+## Base Service
+### Nginx
+For all http/https request proxy
+
+### MySQL
+For all MySQL Database Usage
+
+## Appending Service
+### PHP
+For run composer install or something in cli
---
+++ update-env-example.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script is can update .env.example with the current values from .env
+# Replace all values in .env env value with empty string
+
+# Clear the file .env.example
+> .env.example
+
+# Global variable to store the next line to run
+nextLineDefault=''
+# Read the .env file line by line
+cat .env | while read -r line ; do
+    
+    # init output variable
+    output=$line
+
+    # If this is not comment line, handle it
+    if [[ $line != "#"* ]]; then
+        # If the line has value like FOO=BAR will cut the value FOO=
+        if [[ $line == *"="* ]]; then
+            output=$(echo $line | cut -d '=' -f 1)'='
+        fi
+    fi
+
+    # Output to the .env.example file
+    echo "$output$nextLineDefault" >> .env.example
+
+    # If has default value comment '# Default=value #', will store 'value' for next line
+    if [[ $line == *"Default="* ]]; then
+        nextLineDefault=$(echo $line | cut -d '=' -f 2 | cut -d '#' -f 1)
+    else
+        # clear the next line default value
+        nextLineDefault=''
+    fi
+done
